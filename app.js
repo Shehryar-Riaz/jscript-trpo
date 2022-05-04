20e67b92946f322ade15fa1afae7eef7274b756d
Shehryar <sirshehryar26@gmail.com>
2022-05-04 07:18:17 +0500

    jscript

---
+++ app.js
@@ -0,0 +1,22 @@
+
+// var name1 = "idrees";
+// document.write(name1)
+
+// var plus = "5" + 45;
+// document.write(plus);
+
+
+
+// var perOperator = 25 % 5;
+// document.write(perOperator);
+
+
+// var pro = prompt("Ali baba");
+// document.write(perOperator);
+
+ var address = prompt("Where Is Tomb Of Quiad-E-Azam?");
+ if(address === "karachi"){
+       alert("You are right")
+ }else{
+       alert("you are wrong")
+ }
